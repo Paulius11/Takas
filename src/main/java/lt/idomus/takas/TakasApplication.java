5f1d30f39821a1dfcf37be1b4623a7b53de8baa0
Paulius11 <paulius.gaizauskas@gmail.com>
2020-11-11 15:23:32 +0200

    Innitial commit

---
+++ src/main/java/lt/idomus/takas/TakasApplication.java
@@ -0,0 +1,13 @@
+package lt.idomus.takas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/idomus/takas/TakasApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.idomus.takas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
