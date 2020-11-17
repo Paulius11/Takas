38cf8bc2d5667710ab6ed90168f668de0393cc50
iamEJ <jukneviciute185@gmail.com>
2020-11-17 17:55:26 +0200

    add and style header and jumbotron component

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/header/Header";
+import Jumbotron from "../components/header/jumbotron/Jumbotron";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Jumbotron />
+    </div>
+  );
+}
+
+export default Home;
