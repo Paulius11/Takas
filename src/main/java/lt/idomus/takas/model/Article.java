6962ddf35f4f0f2817b86de3692c3fe41e2f1e8e
vilksp <v.kasperavicius1@gmail.com>
2020-11-23 15:39:03 +0200

    Article class got added lombok annotations to reduce boilerplate code.

--- src/main/java/lt/idomus/takas/model/Article.java
+++ src/main/java/lt/idomus/takas/model/Article.java
@@ -1,8 +1,15 @@
 package lt.idomus.takas.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,102 +23,6 @@ public class Article {
     private double length;
     private String image;
 
-    public Article() {
-    }
-
-    public Article(String title, String description, boolean featured, int rating, String difficulty, String region, double length, String image) {
-        super();
-        this.title = title;
-        this.description = description;
-        this.featured = featured;
-        this.rating = rating;
-        this.difficulty = difficulty;
-        this.region = region;
-        this.length = length;
-        this.image = image;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setFeatured(boolean featured) {
-        this.featured = featured;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public void setDifficulty(String difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public boolean isFeatured() {
-        return featured;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public String getDifficulty() {
-        return difficulty;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public double getLength() {
-        return length;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                '}';
-    }
 
 }
 
