060f676f0db0e518078a23a259d933cb549f7a26
vilksp <v.kasperavicius1@gmail.com>
2020-11-23 15:26:48 +0200

    Lombok added, ArticleRepository @repository annotation added

--- src/main/java/lt/idomus/takas/doa/ArticleRepository.java
+++ src/main/java/lt/idomus/takas/doa/ArticleRepository.java
@@ -2,6 +2,8 @@ package lt.idomus.takas.doa;
 
 import lt.idomus.takas.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository  extends JpaRepository<Article, Long> {
 }
