88c0a2ab6ae5bffe1fd4839cd764dff17e6b9cb6
Paulius11 <paulius.gaizauskas@gmail.com>
2020-11-24 07:41:34 +0200

    Add swager

---
+++ src/main/java/lt/idomus/takas/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package lt.idomus.takas;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(metadata()).select()
+                .apis(RequestHandlerSelectors.basePackage("lt.idomus.takas")).build();
+    }
+
+    public ApiInfo metadata() {
+        return new ApiInfoBuilder().title("Takai").description("Keliai ir takeliai")
+                .version("1.0").build();
+    }
+
+}
