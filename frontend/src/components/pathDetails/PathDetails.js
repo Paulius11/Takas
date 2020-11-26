fb80650d3f368ecc57a53b51cc53ece79734bfa8
iamEJ <jukneviciute185@gmail.com>
2020-11-26 16:18:53 +0200

    add path details header component

--- frontend/src/components/pathDetails/PathDetails.js
+++ frontend/src/components/pathDetails/PathDetails.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { PathContext } from "../../context/PathProvider";
 import Loading from "../loading/Loading";
 import "./PathDetails.css";
+import PathDetailsHeader from "../pathDetailsHeader.js/PathDetailsHeader";
 
 function PathDetails() {
   const { id } = useParams();
@@ -24,9 +25,7 @@ function PathDetails() {
     return (
       <div className="pathDetails">
         <div className="pathDetails__container">
-          <h1>{id}</h1>
-          <Link to={`/paths/${id}/photos`}>Photos</Link>
-          <h1>{title}</h1>
+          <PathDetailsHeader title={title} id={id} />
           <h2>{rating}</h2>
           <h3>{difficulty}</h3>
           <h4>{length}</h4>
