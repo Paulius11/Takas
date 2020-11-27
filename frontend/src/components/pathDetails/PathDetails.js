398b59de60376a8e0cb8bba47ced7299282c7506
iamEJ <jukneviciute185@gmail.com>
2020-11-27 13:50:17 +0200

    add path style

--- frontend/src/components/pathDetails/PathDetails.js
+++ frontend/src/components/pathDetails/PathDetails.js
@@ -4,6 +4,7 @@ import { PathContext } from "../../context/PathProvider";
 import Loading from "../loading/Loading";
 import "./PathDetails.css";
 import PathDetailsHeader from "../pathDetailsHeader.js/PathDetailsHeader";
+import StarRating from "../starRating/StarRating";
 
 function PathDetails() {
   const { id } = useParams();
@@ -26,12 +27,44 @@ function PathDetails() {
       <div className="pathDetails">
         <div className="pathDetails__container">
           <PathDetailsHeader title={title} id={id} />
-          <h2>{rating}</h2>
-          <h3>{difficulty}</h3>
-          <h4>{length}</h4>
-          <h5>{region}</h5>
-          <p>{description}</p>
-          <img alt={title} src={image} />
+          <div className="pathDetails__body">
+            <div className="pathDetails__bodyImage">
+              <img alt={title} src={image} />
+            </div>
+            <div className="pathDetails__bodyInfo">
+              <div className="pathDetails__bodyInfoTop">
+                <div>
+                  <h1>{rating}</h1>
+                </div>
+                <div>
+                  <div className="pathDetails__rating">
+                    <p>Your rating:</p>
+                    <StarRating rate={rating} />
+                  </div>
+                  <div className="pathDetails__ratingAll">
+                    <p>Rating:</p>
+                    <span>
+                      <strong>{rating}/5</strong> from <strong>2</strong> users
+                    </span>
+                  </div>
+                  <div className="pathDetails__difficulty">
+                    <p>Difficulty:</p>
+                    <h4>{difficulty}</h4>
+                  </div>
+                  <div className="pathDetails__length">
+                    <p>Length:</p>
+                    <h4>{length} km</h4>
+                  </div>
+                </div>
+              </div>
+              <div className="pathDetails__bodyInfoText">
+                <div className="pathDetails__region">
+                  <h3>{region}</h3>
+                </div>
+                <p>{description}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- frontend/src/pages/Paths.js
+++ frontend/src/pages/Paths.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PathPager from "../components/paths/PathPager";
+import Footer from "../components/footer/Footer";
 
 function Paths() {
   return (
     <div>
       <PathPager />
+      <Footer />
     </div>
   );
 }
