536ea52e51bef769c3055a2af1c6956171dde307
vilksp <v.kasperavicius1@gmail.com>
2020-12-14 19:13:02 +0200

    changed User to Role mapping from onetomany to manytomany

--- src/main/java/lt/idomus/takas/model/ArticleUser.java
+++ src/main/java/lt/idomus/takas/model/ArticleUser.java
@@ -26,7 +26,7 @@ public class ArticleUser {
     private String fullName;
     private String password;
 
-    @OneToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
