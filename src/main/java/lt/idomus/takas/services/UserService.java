a1ea3d1c2ebae9b7dfcfca067b043571a0396a4e
vilksp <v.kasperavicius1@gmail.com>
2020-12-15 19:59:00 +0200

    extracted login method from controller to service layer

--- src/main/java/lt/idomus/takas/services/UserService.java
+++ src/main/java/lt/idomus/takas/services/UserService.java
@@ -7,7 +7,14 @@ import lt.idomus.takas.dto.CreateUserDTO;
 import lt.idomus.takas.exceptions.exception.PasswordDontMatchException;
 import lt.idomus.takas.exceptions.exception.UserAlreadyExistsException;
 import lt.idomus.takas.model.ArticleUser;
+import lt.idomus.takas.model.JwtLoginSuccessResponse;
+import lt.idomus.takas.model.LoginRequest;
 import lt.idomus.takas.model.Role;
+import lt.idomus.takas.security.JwtTokenProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,34 +28,49 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
     private final RoleRepository roleRepository;
-    //Later cache this role so it would save load on server
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider provider;
+
+
+    public JwtLoginSuccessResponse loginAttempt(LoginRequest request) {
+
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = provider.generateToken(authentication);
+
+        return new JwtLoginSuccessResponse(jwt);
+    }
+
 
     public CreateUserDTO createUser(CreateUserDTO userForm) {
 
         ArticleUser user = new ArticleUser();
 
 
+        if (!userForm.getPassword().equals(userForm.getConfirmPassword())) {
+            throw new PasswordDontMatchException("Password's doesn't match!");
+        }
+        //Hashing passwords
 
 
-            if (!userForm.getPassword().equals(userForm.getConfirmPassword())) {
-                throw new PasswordDontMatchException("Password's doesn't match!");
-            }
-            //Hashing passwords
+        Role userRole = repository.findByRole("USER");
+        if (userRole == null) {
+            Role role = new Role();
+            role.setRole("USER");
+            userRole = role;
+            roleRepository.save(role);
+        }
+        user.setUsername(userForm.getUsername());
+        user.setFullName(userForm.getFullName());
+        user.setPassword(encoder.encode(userForm.getPassword()));
+        user.setRoles(Set.of(userRole));
 
-
-            Role userRole = repository.findByRole("USER");
-            if (userRole == null) {
-                Role role = new Role();
-                role.setRole("USER");
-                userRole = role;
-                roleRepository.save(role);
-            }
-            user.setUsername(userForm.getUsername());
-            user.setFullName(userForm.getFullName());
-            user.setPassword(encoder.encode(userForm.getPassword()));
-            user.setRoles(Set.of(userRole));
-
-            userRepository.save(user);
+        userRepository.save(user);
 
         userForm.setPassword("");
         userForm.setConfirmPassword("");
