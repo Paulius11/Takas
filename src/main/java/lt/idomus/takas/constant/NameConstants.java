a67d8278cd8df75cdfd0b05d4cddf28c652a4244
Paulius11 <paulius.gaizauskas@gmail.com>
2021-03-02 07:24:10 +0200

    Refactor coin constant

--- src/main/java/lt/idomus/takas/constant/NameConstants.java
+++ src/main/java/lt/idomus/takas/constant/NameConstants.java
@@ -4,4 +4,6 @@ public class NameConstants {
     public static final String LOGIN_UNAUTHENTICATED = "Username or Password is invalid!";
     public static final String LOGIN_AUTHENTICATED = "Authentication successful!";
     public static final String REGISTRATION_SUCCESSFUL = "Registration successful";
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
 }
