b417c4c411bbe022f3c18a1535e60e0b4a293ebf
Paulius11 <paulius.gaizauskas@gmail.com>
2021-03-02 07:20:28 +0200

    Expire JWT token in 3 days

--- src/main/java/lt/idomus/takas/constant/SecurityConstant.java
+++ src/main/java/lt/idomus/takas/constant/SecurityConstant.java
@@ -6,7 +6,8 @@ public class SecurityConstant {
     public static final String USER_PATH = "/api/user/**";
     public static final String GET_ALL_ARTICLE_LIST = "/api/article/**";
     public static final String SECRET = "E13023670F1DD064E7A5280916B36F1CFD34F2D3386F4944635F536720D4995E";
-    public static final Long EXPIRATION_IN_MILLISECS = 3600000L;
+    public static final Integer EXPIRATION_IN_DAYS = 3; //
+    public static final Long EXPIRATION_IN_MILLISECONDS = 3600000L * 24 * EXPIRATION_IN_DAYS; // 3600000ms = 1h
 
     // Enable Swagger
     public static final String[] SWAGGER_PATH = {"/v2/api-docs",
