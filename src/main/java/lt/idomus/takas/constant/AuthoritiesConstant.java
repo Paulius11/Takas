2a7783d898e3d1beb910adba6d1070ec7139b6cf
Paulius11 <paulius.gaizauskas@gmail.com>
2021-03-03 16:42:34 +0200

    Update security constants

--- src/main/java/lt/idomus/takas/constant/AuthoritiesConstant.java
+++ src/main/java/lt/idomus/takas/constant/AuthoritiesConstant.java
@@ -1,9 +1,25 @@
 package lt.idomus.takas.constant;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class AuthoritiesConstant {
 
-    public static final String[] USER_ROLES =  {"authenticated", "user", "article:read", "article:offer"};
-    public static final String[] MODERATOR =   {"authenticated", "user", "moderator", "article:read", "article:create", "article:update", "article:approve", "article:offer" };
-    public static final String[] ADMIN_ROLES = {"authenticated", "user", "moderator","admin", "article:read", "article:create", "article:update", "article:delete", "article:offer"};
 
+public static final List<String> USER_ROLES = Arrays.asList("authenticated", "user", "article:read", "article:offer");
+public static final List<String> MODERATOR_ROLES = Stream.concat(USER_ROLES.stream(),
+        Stream.of( "moderator", "article:create", "article:update", "article:approve"))
+        .collect(Collectors.toUnmodifiableList());
+public static final List<String> ADMIN_ROLES = Stream.concat(MODERATOR_ROLES.stream(),
+        Stream.of("admin", "article:delete"))
+        .collect(Collectors.toUnmodifiableList());
+
+    public static void main ( String[] args )
+    {
+        System.out.println( "USER_ROLES = " + USER_ROLES );
+        System.out.println( "MODERATOR_ROLES = " + MODERATOR_ROLES );
+        System.out.println( "ADMIN_ROLES = " + ADMIN_ROLES );
+    }
 }
--- src/main/java/lt/idomus/takas/enums/Role.java
+++ src/main/java/lt/idomus/takas/enums/Role.java
@@ -1,21 +1,23 @@
 package lt.idomus.takas.enums;
 
+import java.util.List;
+
 import static lt.idomus.takas.constant.AuthoritiesConstant.*;
 
 public enum Role {
 
     ROLE_USER(USER_ROLES),
-    ROLE_SUPER_ADMIN(MODERATOR),
+    ROLE_SUPER_ADMIN(MODERATOR_ROLES),
     ROLE_ADMIN(ADMIN_ROLES);
 
-    private String[] authorities;
+    private List<String> authorities;
 
-    Role(String... authority) {
+    Role(List<String> authority) {
         this.authorities = authority;
     }
 
     public String[] getAuthorities() {
-        return authorities;
+        return authorities.toArray(new String[0]);
     }
 
 }
