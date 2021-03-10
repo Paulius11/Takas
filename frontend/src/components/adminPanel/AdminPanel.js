ce8f6fae5b8ac2306eb742b60ecbeafc12696cd0
Egle J <jukneviciute185@gmail.com>
2021-03-10 14:12:05 +0200

    add users and paths list in admin panel

--- frontend/src/components/adminPanel/AdminPanel.js
+++ frontend/src/components/adminPanel/AdminPanel.js
@@ -1,12 +1,15 @@
 import React from "react";
 import AdminSidebar from "./AdminSidebar";
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
+import UsersList from "../adminComponets/users/UsersList";
+import PathsList from "../adminComponets/paths/PathsList";
 
 // Example components
 const AdminHome = () => {
   return (
     <div style={{ flex: 0.8 }}>
       <h1>Admin Home</h1>
+      <Link to="/admin-panel/data/paths">Paths</Link>
     </div>
   );
 };
@@ -27,22 +30,6 @@ const DashboardSettings = () => {
   );
 };
 
-const DataUsers = () => {
-  return (
-    <div style={{ flex: 0.8 }}>
-      <h1>Users</h1>
-    </div>
-  );
-};
-
-const DataPaths = () => {
-  return (
-    <div style={{ flex: 0.8 }}>
-      <h1>Paths</h1>
-    </div>
-  );
-};
-
 function AdminPanel() {
   return (
     <div
@@ -50,22 +37,26 @@ function AdminPanel() {
         display: "flex",
       }}
     >
-      <AdminSidebar />
-      <Route path="/admin-panel/admin-home">
-        <AdminHome />
-      </Route>
-      <Route path="/admin-panel/dashboard/statistics">
-        <DashboardStatistics />
-      </Route>
-      <Route path="/admin-panel/dashboard/settings">
-        <DashboardSettings />
-      </Route>
-      <Route path="/admin-panel/data/users">
-        <DataUsers />
-      </Route>
-      <Route path="/admin-panel/data/paths">
-        <DataPaths />
-      </Route>
+      <div style={{ flex: "0.2" }}>
+        <AdminSidebar />
+      </div>
+      <div style={{ flex: "0.8", background: "#efefef" }}>
+        <Route path="/admin-panel/admin-home">
+          <AdminHome />
+        </Route>
+        <Route path="/admin-panel/dashboard/statistics">
+          <DashboardStatistics />
+        </Route>
+        <Route path="/admin-panel/dashboard/settings">
+          <DashboardSettings />
+        </Route>
+        <Route path="/admin-panel/data/users">
+          <UsersList />
+        </Route>
+        <Route exact path="/admin-panel/data/paths">
+          <PathsList />
+        </Route>
+      </div>
     </div>
   );
 }
