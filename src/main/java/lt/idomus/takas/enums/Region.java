54eddd4f8c3f6527cb11f3ead30b5065a0c53b63
Paulius11 <paulius.gaizauskas@gmail.com>
2021-03-22 21:14:46 +0200

    Fix typo

--- src/main/java/lt/idomus/takas/enums/Region.java
+++ src/main/java/lt/idomus/takas/enums/Region.java
@@ -1,5 +1,5 @@
 package lt.idomus.takas.enums;
 
 public enum Region {
-    VILNIUS, KAUNAS, KLAIPĖDA, MARIAMPOLĖ, PANEVĖŽYS, ŠIAULIAI, TAURAGĖ, TELŠIAI, UTENA
+    VILNIUS, KAUNAS, KLAIPĖDA, MARIJAMPOLĖ, PANEVĖŽYS, ŠIAULIAI, TAURAGĖ, TELŠIAI, UTENA
 }
