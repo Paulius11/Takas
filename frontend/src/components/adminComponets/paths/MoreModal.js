e7e15a581fe086224a7fccc5c8d7cc9e0683bd70
iamEJ <jukneviciute185@gmail.com>
2021-03-25 17:07:24 +0200

    change moreModal style

--- frontend/src/components/adminComponets/paths/MoreModal.js
+++ frontend/src/components/adminComponets/paths/MoreModal.js
@@ -23,39 +23,42 @@ function MoreModal({ isModalOpen, closeModal, passToModal }) {
     >
       <div className="modal-container">
         <h1>More info:</h1>
-        <div className="more__info">
-          <h4>Title:</h4>
-          <p>{title}</p>
+        <div className="modal__infoConatainer">
+          <div className="more__info">
+            <h4>Title:</h4>
+            <p>{title}</p>
+          </div>
+          <div className="more__info">
+            <h4>Length:</h4>
+            <p>{length}</p>
+          </div>
+          <div className="more__info">
+            <h4>Difficulty:</h4>
+            <p>{difficulty}</p>
+          </div>
+          <div className="more__info">
+            <h4>Rating:</h4>
+            <p>{rating}</p>
+          </div>
+          <div className="more__info">
+            <h4>Region:</h4>
+            <p>{region}</p>
+          </div>
+          <div className="more__info">
+            <h4>Featured:</h4>
+            <p>{featured ? `Yes` : `No`}</p>
+          </div>
+          <div className="more__info">
+            <h4>Published:</h4>
+            <p>{published ? `Yes` : `No`}</p>
+          </div>
         </div>
-        <div className="more__info">
-          <h4>Length:</h4>
-          <p>{length}</p>
-        </div>
-        <div className="more__info">
-          <h4>Difficulty:</h4>
-          <p>{difficulty}</p>
-        </div>
-        <div className="more__info">
-          <h4>Rating:</h4>
-          <p>{rating}</p>
-        </div>
-        <div className="more__info">
-          <h4>Region:</h4>
-          <p>{region}</p>
-        </div>
-        <div className="more__info">
-          <h4>Featured:</h4>
-          <p>{featured ? `Yes` : `No`}</p>
-        </div>
-        <div className="more__info">
-          <h4>Published:</h4>
-          <p>{published ? `Yes` : `No`}</p>
-        </div>
-        <div className="more__info">
+
+        <div className="more__description">
           <h4>Description:</h4>
           <p>{description}</p>
         </div>
-        <div className="more__info">
+        <div className="more__image">
           <img src={image} alt={title} />
         </div>
         <button className="close-modal-btn" onClick={closeModal}>
